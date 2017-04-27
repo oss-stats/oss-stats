5ee419e1569883b9418ca71517fe56ad327eb121
Gary Ewan Park <gep13@gep13.co.uk>
2017-04-27 13:22:34 +0100

    Update run.ps1

--- NuGetStats/run.ps1
+++ NuGetStats/run.ps1
@@ -36,7 +36,8 @@ $packages = @(
     'Cake.ImageOptimizer',              'Cake.AWS.CodeDeploy',              'Cake.MSBuildTask',             'Cake.ResxConverter',
     'Cake.UncUtils',                    'Cake.VsixSignTool'                 'Cake.EntityFramework',         'Cake.Netlify',
     'Cake.Azure',                       'SemVer.FromAssembly',              'Cake.SemVer.FromAssembly',     'Cake.SemVer.FromBinary',
-    'Cake.SynVer',                      'Cake.DeployParams',                'Cake.DocCreator',              'Cake.BuildSystems.Module'
+    'Cake.SynVer',                      'Cake.DeployParams',                'Cake.DocCreator',              'Cake.BuildSystems.Module',
+    'Cake.DNF.Module'
 )
 
 $batched    = [System.DateTimeOffset]::Now
