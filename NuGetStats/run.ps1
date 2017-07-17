cafaccbc1e0870b275a042ea108a5c43d1e0ca20
Gary Ewan Park <gep13@gep13.co.uk>
2017-07-17 15:18:42 +0100

    Update run.ps1

--- NuGetStats/run.ps1
+++ NuGetStats/run.ps1
@@ -42,7 +42,7 @@ $packages = @(
     'Cake.PinNuGetDependency',          'Cake.FileSet',                     'Cake.Yeoman',                      'Cake.Wyam.Recipe',                
     'Cake.Gradle',                      'Cake.Netlify',                     'Cake.UncUtils',                    'Cake.AzCopy',                     
     'Cake.Chocolatey.Module',           'Cake.Hosts',                       'Cake.KeePass',                     'Cake.SonarScanner',               
-    'Cake.Parallel.Module',             'Cake.Bumpy',                       'Cake.ArgumentHelpers'
+    'Cake.Parallel.Module',             'Cake.Bumpy',                       'Cake.ArgumentHelpers',             'Cake.DocumentDb'
 )
 
 $batched    = [System.DateTimeOffset]::Now
